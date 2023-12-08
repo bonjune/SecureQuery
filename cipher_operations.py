00eb6d8a4351721beb488f4caa110bf39eeef6c1
Bongjun Jang <bongjun.jang@kaist.ac.kr>
2023-12-08 01:10:04 -0500

    add perf analysis

---
+++ cipher_operations.py
@@ -0,0 +1,76 @@
+from Pyfhel import Pyfhel, PyCtxt
+import glob
+from hashlib import sha256
+import pandas as pd
+
+
+def init_scheme():
+    ckks_settings = glob.glob("ckks-*")
+    if len(ckks_settings) == 5:
+        HE = Pyfhel()
+        HE.load_context("ckks-context")
+        HE.load_public_key("ckks-pubkey")
+        HE.load_secret_key("ckks-seckey")
+        HE.load_relin_key("ckks-relinkey")
+        HE.load_rotate_key("ckks-rotate-key")
+    else:
+        HE = Pyfhel()
+        HE.contextGen(
+            scheme="ckks", n=2**14, scale=2**30, qi_sizes=[60, 30, 30, 30, 60]
+        )
+        HE.keyGen()
+        HE.relinKeyGen()
+        HE.rotateKeyGen()
+
+        HE.save_context("ckks-context")
+        HE.save_public_key("ckks-pubkey")
+        HE.save_secret_key("ckks-seckey")
+        HE.save_relin_key("ckks-relinkey")
+        HE.save_rotate_key("ckks-rotate-key")
+
+    return HE
+
+
+def hash_column_name(name: str):
+    return sha256(name.encode()).hexdigest()
+
+
+def encrypted_data(encryptor: Pyfhel, data: pd.DataFrame):
+    enc_data = {}
+
+    for col_name in data.columns:
+        hashed_col_name = hash_column_name(col_name)
+        col_data = data[col_name].to_numpy(copy=True)
+        data_ctxt = encryptor.encrypt(col_data)
+        enc_data[hashed_col_name] = (data_ctxt, len(col_data))
+
+    return enc_data
+
+
+def cipher_sum(ctxt: PyCtxt, size: int):
+    if size <= 1:
+        return ctxt
+
+    fold = size // 2
+    if size % 2 == 0:
+        return cipher_sum(ctxt + (ctxt << fold), fold)
+
+    hold = ctxt.copy()
+    return (hold << (size - 1)) + cipher_sum(ctxt + (ctxt << fold), fold)
+
+
+def cipher_average(ctxt: PyCtxt, size: int):
+    avg_cipher = cipher_sum(ctxt, size) / size
+    return avg_cipher
+
+
+def cipher_inner_product(ctxt1: PyCtxt, ctxt2: PyCtxt, size: int):
+    ctxt_mul = ~(ctxt1 * ctxt2)  # relinearization to reduce size
+    return cipher_sum(ctxt_mul, size)
+
+
+def cipher_covariance(ctxt1: PyCtxt, ctxt2: PyCtxt, size: int):
+    x_mean = cipher_average(ctxt1, size)
+    y_mean = cipher_average(ctxt2, size)
+
+    return cipher_inner_product(ctxt1 - x_mean, ctxt2 - y_mean, size) / size
--- main.py
+++ main.py
@@ -14,17 +14,17 @@ def init():
 
 
 HE = init()
-data = pd.read_csv("data.csv")
+# data = pd.read_csv("data.csv")
 
-ogm = data[" Operating Gross Margin"].to_numpy(dtype=np.float32, copy=True)
+# ogm = data[" Operating Gross Margin"].to_numpy(dtype=np.float32, copy=True)
 
-ogm_sum = ogm.sum()
+# ogm_sum = ogm.sum()
 
-ogm_holder = HE.encrypt(0.0)
-for record in ogm:
-    ogm_holder += HE.encrypt(record)
-ogm_sum_cipher = ogm_holder
-ogm_sum_dec = ogm_sum_cipher.decrypt()
+# ogm_holder = HE.encrypt(0.0)
+# for record in ogm:
+# ogm_holder += HE.encrypt(record)
+# ogm_sum_cipher = ogm_holder
+# ogm_sum_dec = ogm_sum_cipher.decrypt()
 
-print(ogm_sum)
-print(ogm_sum_dec)
+# print(ogm_sum)
+# print(ogm_sum_dec)
