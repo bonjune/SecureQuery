42ffe089914efecfc4968d92eb1d9fb11b570650
thejollyfatso <rafael.r.deleon@gmail.com>
2023-12-07 17:59:53 -0800

    calculation of mean made into a function

--- correlation.py
+++ correlation.py
@@ -13,24 +13,27 @@ def init():
     return HE
 
 
+def mean(HE, data_vec):
+    data_holder = HE.encrypt(0.0)
+    records = 0
+    for record in data_vec:
+        data_holder += HE.encrypt(record)
+        records += 1
+    data_sum_c = data_holder
+    data_mean_c = data_sum_c / records
+    return data_mean_c
+
+
 def main():
     HE = init()
     data = pd.read_csv("data.csv")
 
     ogm = data[" Operating Gross Margin"].to_numpy(dtype=np.float32, copy=True)
-    ogm_sum = ogm.sum()
 
-    ogm_holder = HE.encrypt(0.0)
-    records = 0
-    for record in ogm:
-        ogm_holder += HE.encrypt(record)
-        records += 1
-    ogm_sum_c = ogm_holder
-    ogm_mean_c = ogm_sum_c / records
+    ogm_mean_c = mean(HE, ogm)
     ogm_op_dec = ogm_mean_c.decrypt()
 
     print(ogm_op_dec)
-    print(ogm_sum / records)
     return 0
 
 
